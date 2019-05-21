3ca085c894ca5d29d19137e767e7223a937f7c3e
sophia wu <sophia6734wu@gmail.com>
2019-05-21 16:23:38 +1000

    Wikinonimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+puts 'seeding'
 # Examples:
-#
+10.times do
+  Article.create(
+    title: Faker::Lorem.word,
+    content: Faker::Lorem.sentence,
+    author: Faker::Lorem.word
+  )
+end
+
+puts 'end of seeding'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
